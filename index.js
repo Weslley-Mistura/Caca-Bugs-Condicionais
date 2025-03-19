d4464f9ec58c4ae941451cd2af947c0a74e8cf71
Weslley Mistura <weslleymisturadev@gmail.com>
2025-03-18 21:26:17 -0300

    alterações no JS para corrigir funcionalidades

--- index.js
+++ index.js
@@ -2,19 +2,23 @@
 
 let iniciar = prompt("Deseja iniciar o programa? s/n")
 
-if (iniciar == "s") {
+if (iniciar == "n") {
     alert("Programa encerrado!")
 } else {
     alert("Programa iniciado!")
-    let opcao = prompt("Escolha uma ação a ser feita -> 1 - Mensagem motivacional; 2 - Mensagem desmotivacional; 3 - nada")
-    switch (iniciar) {
+    let opcao = Number(prompt("Escolha uma ação a ser feita -> \n 1 - Mensagem motivacional; \n 2 - Mensagem desmotivacional; \n 3 - nada"))
+    switch (opcao) {
         case 1:
-            alert("Você NÃO VAI conseguir, desista!")
-        case 2:
             alert("Continue tentando, você VAI conseguir!")
+            break;
+        case 2:
+            alert("Você NÃO VAI conseguir, desista!")
+            break;
         case 3:
-            alert("Opção inválida selecione um número de 1 a 3.")
-        default:
             alert("...")
+            break;
+        default:
+            alert("Opção inválida selecione um número de 1 a 3.")
+            break;
     }
 }
